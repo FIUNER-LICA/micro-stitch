300c613ce4ce5073111ae04c6c05b7214f709244
unknown <jetchart@ingenieria.uner.edu.ar>
2022-04-27 13:11:33 -0300

    se crea un nuevo branch llamado image-defocus y se sube la carpeta image-defocus

---
+++ image-defocus/mainwindow_ui.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(502, 356)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
+        self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setObjectName("centralwidget")
+        self.labelImage = QtWidgets.QLabel(self.centralwidget)
+        self.labelImage.setGeometry(QtCore.QRect(10, 10, 321, 281))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.labelImage.sizePolicy().hasHeightForWidth())
+        self.labelImage.setSizePolicy(sizePolicy)
+        self.labelImage.setLineWidth(1)
+        self.labelImage.setMidLineWidth(0)
+        self.labelImage.setObjectName("labelImage")
+        self.labelInfo = QtWidgets.QLabel(self.centralwidget)
+        self.labelInfo.setGeometry(QtCore.QRect(360, 10, 131, 281))
+        self.labelInfo.setObjectName("labelInfo")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 502, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuArchivo = QtWidgets.QMenu(self.menubar)
+        self.menuArchivo.setObjectName("menuArchivo")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionCerrar = QtWidgets.QAction(MainWindow)
+        self.actionCerrar.setObjectName("actionCerrar")
+        self.menuArchivo.addAction(self.actionCerrar)
+        self.menubar.addAction(self.menuArchivo.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.labelImage.setText(_translate("MainWindow", "TextLabel"))
+        self.labelInfo.setText(_translate("MainWindow", "TextLabel"))
+        self.menuArchivo.setTitle(_translate("MainWindow", "Archivo"))
+        self.actionCerrar.setText(_translate("MainWindow", "Cerrar"))
+
+
