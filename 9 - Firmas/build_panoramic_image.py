f346f52b723a1bb9093139ef837c9d2ad0d5065b
Santiago Fiotto <santiago.a.fiotto@gmail.com>
2022-05-13 13:42:32 -0300

    Se elimina el directorio '9 - Firmas'

--- 9 - Firmas/build_panoramic_image.py
+++
@@ -1,8 +0,0 @@
-def store_valid_images(image):
-    pass
-
-def overlap_sector_combination_replacement(panoramic,image, F, C, tras_f, tras_c):
-    pass
-
-def overlap_sector_combination_linear_interp(panoramic,image, F, C, tras_f, tras_c):
-    pass
--- 9 - Firmas/mask_quest.py
+++
@@ -1,8 +0,0 @@
-from threading import Thread
-from mask_extracting import Mask
-
-def default_search(image, mask: Mask) -> None: # lista de 1 elemento
-    pass
-
-def compound_search(image, mask: Mask): # lista de más de 1 elemento
-    pass
