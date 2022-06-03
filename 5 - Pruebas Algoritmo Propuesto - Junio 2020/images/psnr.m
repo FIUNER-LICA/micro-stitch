f73f9b07e6c97ca17046912ddf177e0356dbc63c
Santiago Fiotto <santiago.a.fiotto@gmail.com>
2022-06-03 16:15:30 -0300

    LIMPIEZA DE REPOSITORIO. Se eliminan los directorios correspondientes a las implementaciones y experimentos previos a la reestructuración del proyecto.

--- 5 - Pruebas Algoritmo Propuesto - Junio 2020/images/psnr.m
+++
@@ -1,5 +0,0 @@
-function PSNR = psnr(OR_IMAGE,MOD_IMAGE,MAX_INTENSITY)
-% usage: psnr(OR_IMAGE,MOD_IMAGE,MAX_INTENSITY)
-         MSE=mean(((OR_IMAGE(:)-MOD_IMAGE(:)).^2));
-         PSNR=20*log10(MAX_INTENSITY/sqrt(MSE));
-end
