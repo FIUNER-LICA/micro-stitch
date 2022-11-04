b6acde3443edf90b567360e3349f3bbfbb622962
Santiago F <sandres.f@outlook.com>
2022-11-04 16:04:37 -0300

    Se homogenizan los providers de imagenes por un lado y de panorámicas por otro. Se construye la panorámica con el mismo objeto para ambas cámaras, por lo que en futuras pruebas no tendrían que surgir errores, y se podría refactorizar. Queda listo para probar si la captura con pyspin se realiza correctamente.

--- apps/app_module_spinnaker.py
+++ apps/app_module_spinnaker.py
@@ -7,21 +7,26 @@ import modules.panoramic_acquisition as pac
 from modules.globals_DTO import *
 import modules.frame_validation as f_val
 
-import cv2
 import numpy as np
-import datetime
 from threading import Event, Thread
 
 import PySpin
-import keyboard
-
 
 class AppSpinnaker:
     global continue_recording
     continue_recording = True
 
     def __init__(self) -> None:
-        
+        # Search Camera
+        self.cam_list = []
+        self.num_cameras = 0
+        self.system = None
+        # Run camera
+        self.cam = None
+        # Aquire image
+        self.nodemap = None
+        self.nodemap_tldevice = None
+
     # Bandera para activar/desactivar el pre-análisis
         self.focus = True
 
@@ -68,7 +73,7 @@ class AppSpinnaker:
         continue_recording = False
 
 
-    def acquire_and_display_images(self, cam, nodemap, nodemap_tldevice):
+    def acquire_and_display_images(self):#, cam, nodemap, nodemap_tldevice):
         """
         This function continuously acquires images from a device and display them in a GUI.
 
@@ -81,7 +86,7 @@ class AppSpinnaker:
         :return: True if successful, False otherwise.
         :rtype: bool
         """
-        sNodemap = cam.GetTLStreamNodeMap() # Código para detección de cámara - excepción
+        sNodemap = self.cam.GetTLStreamNodeMap() # Código para detección de cámara - excepción
 
         # Change bufferhandling mode to NewestOnly
         node_bufferhandling_mode = PySpin.CEnumerationPtr(sNodemap.GetNode('StreamBufferHandlingMode'))
@@ -103,7 +108,7 @@ class AppSpinnaker:
 
         print('*** IMAGE ACQUISITION ***\n')
         try:
-            node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+            node_acquisition_mode = PySpin.CEnumerationPtr(self.nodemap.GetNode('AcquisitionMode'))
             if not PySpin.IsAvailable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
                 print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
                 return False
@@ -133,7 +138,7 @@ class AppSpinnaker:
             #
             #  *** LATER ***
             #  Image acquisition must be ended when no more images are needed.
-            cam.BeginAcquisition()
+            self.cam.BeginAcquisition()
 
             print('Acquiring images...')
 
@@ -144,7 +149,7 @@ class AppSpinnaker:
             #  overwriting one another. Grabbing image IDs could also accomplish
             #  this.
             device_serial_number = ''
-            node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+            node_device_serial_number = PySpin.CStringPtr(self.nodemap_tldevice.GetNode('DeviceSerialNumber'))
             if PySpin.IsAvailable(node_device_serial_number) and PySpin.IsReadable(node_device_serial_number):
                 device_serial_number = node_device_serial_number.GetValue()
                 print('Device serial number retrieved as %s...' % device_serial_number)
@@ -160,7 +165,6 @@ class AppSpinnaker:
 
             # Retrieve and display images
 
-            # numero_frame_stack = 0
             try:
                 #  Retrieve next received image
                 #
@@ -173,7 +177,7 @@ class AppSpinnaker:
                 #  needed, the image must be released in order to keep the
                 #  buffer from filling up.
                 
-                image_result = cam.GetNextImage(1000)
+                image_result = self.cam.GetNextImage(1000)
 
                 #  Ensure image completion
                 if image_result.IsIncomplete():
@@ -188,13 +192,6 @@ class AppSpinnaker:
             except PySpin.SpinnakerException as ex:
                 print('Error: %s' % ex)
                 return False
-
-            #  End acquisition
-            #
-            #  *** NOTES ***
-            #  Ending acquisition appropriately helps ensure that devices clean up
-            #  properly and do not need to be power-cycled to maintain integrity.
-            cam.EndAcquisition()
 
         except PySpin.SpinnakerException as ex:
             print('Error: %s' % ex)
@@ -224,54 +221,6 @@ class AppSpinnaker:
         
         return self.panoramic, self.growing
 
-                    # if flag_view:
-                    #     pan_screen = cv2.resize(panoramic,(640,480))
-                    #     frame_screen=cv2.resize(new_image,(640,480))
-                    #     frame_screen = cv2.cvtColor(frame_screen, cv2.COLOR_RGB2BGR)
-                    #     pan_screen = cv2.cvtColor(pan_screen,cv2.COLOR_RGB2BGR)
-                    #     hor_cat = cv2.hconcat([frame_screen,pan_screen])
-                    #     cv2.imshow('Resultado con camara FLIR',hor_cat)
-
-                    # if cv2.waitKey(1) & 0xFF == ord('p'):
-                    #     x = datetime.datetime.now()
-                    #     cv2.imwrite('../data/panoramic_flir_{}_{}_{}_{}_{}.jpg'.format(x.hour,
-                    #                                         x.minute,x.day,x.month, x.year), panoramic[:,:,:])
-                    #     continue_recording=True             
-                    #     print ("Seguir con adquisición")
-                    # if keyboard.is_pressed('ENTER'):
-                    #     print('Program is closing...')
-                        
-                    #     # Close figure
-                    #     input('Done! Press Enter to exit...')
-                    #     cv2.destroyAllWindows()
-                    #     continue_recording=False             
-
-                #  Release image
-                #
-                #  *** NOTES ***
-                #  Images retrieved directly from the camera (i.e. non-converted
-                #  images) need to be released in order to keep from filling the
-                #  buffer.
-        # # #         image_result.Release()
-
-        # # #     except PySpin.SpinnakerException as ex:
-        # # #         print('Error: %s' % ex)
-        # # #         return False
-
-        # # #     #  End acquisition
-        # # #     #
-        # # #     #  *** NOTES ***
-        # # #     #  Ending acquisition appropriately helps ensure that devices clean up
-        # # #     #  properly and do not need to be power-cycled to maintain integrity.
-        # # #     cam.EndAcquisition()
-
-        # # # except PySpin.SpinnakerException as ex:
-        # # #     print('Error: %s' % ex)
-        # # #     return False
-
-        # # # return True
-
-
     def run_single_camera(self, cam):
         """
         This function acts as the body of the example; please see NodeMapInfo example
@@ -282,31 +231,26 @@ class AppSpinnaker:
         :return: True if successful, False otherwise.
         :rtype: bool
         """
+        self.cam = cam
         try:
             result = True
 
-            nodemap_tldevice = cam.GetTLDeviceNodeMap()
+            self.nodemap_tldevice = self.cam.GetTLDeviceNodeMap()
 
             # Initialize camera
-            cam.Init()
+            self.cam.Init()
 
             # Retrieve GenICam nodemap
-            nodemap = cam.GetNodeMap()
+            self.nodemap = self.cam.GetNodeMap()
 
             # Configurando formato de imagen
-            if cam.PixelFormat.GetAccessMode() == PySpin.RW:
-                cam.PixelFormat.SetValue(PySpin.PixelFormat_RGB8)
-                print('Pixel format set to %s...' % cam.PixelFormat.GetCurrentEntry().GetSymbolic())
+            if self.cam.PixelFormat.GetAccessMode() == PySpin.RW:
+                self.cam.PixelFormat.SetValue(PySpin.PixelFormat_RGB8)
+                print('Pixel format set to %s...' % self.cam.PixelFormat.GetCurrentEntry().GetSymbolic())
 
             else:
                 print('Pixel format not available...')
                 result = False
-
-            # Acquire images
-            result &= self.acquire_and_display_images(cam, nodemap, nodemap_tldevice)
-
-            # Deinitialize camera
-            cam.DeInit()
 
         except PySpin.SpinnakerException as ex:
             print('Error: %s' % ex)
@@ -356,27 +300,27 @@ class AppSpinnaker:
         print('Library version: %d.%d.%d.%d' % (version.major, version.minor, version.type, version.build))
 
         # Retrieve list of cameras from the system
-        cam_list = system.GetCameras() #TODO: 
+        self.cam_list = system.GetCameras() #TODO: 
 
-        num_cameras = cam_list.GetSize()
+        self.num_cameras = self.cam_list.GetSize()
 
-        print('Number of cameras detected: %d' % num_cameras)
+        print('Number of cameras detected: %d' % self.num_cameras)
 
         # Finish if there are no cameras
-        # # # # if num_cameras == 0:
+        if self.num_cameras == 0:
 
-        # # # #     # Clear camera list before releasing system
-        # # # #     cam_list.Clear()
+            # Clear camera list before releasing system
+            self.cam_list.Clear()
 
-        # # # #     # Release system instance
-        # # # #     system.ReleaseInstance()
+            # Release system instance
+            system.ReleaseInstance()
 
-        # # # #     print('Not enough cameras!')
-        # # # #     input('Done! Press Enter to exit...')
-        # # # #     return False
+            print('Not enough cameras!')
+            # input('Done! Press Enter to exit...')
+            return False
 
         # Run example on each camera
-        for i, cam in enumerate(cam_list):
+        for i, cam in enumerate(self.cam_list):
 
             print('Running example for camera %d...' % i)
 
@@ -389,15 +333,39 @@ class AppSpinnaker:
         # The usage of del is preferred to assigning the variable to None.
         # # # del cam
 
-        # Clear camera list before releasing system
-        cam_list.Clear()
+        # # Clear camera list before releasing system
+        # self.cam_list.Clear()
 
-        # Release system instance
-        system.ReleaseInstance()
+        # # Release system instance
+        # system.ReleaseInstance()
 
         # input('Done! Press Enter to exit...')
         return result
 
+    def read(self):
+        # Acquire images
+        result = True
+        result &= self.acquire_and_display_images()#self.cam, self.nodemap, self.nodemap_tldevice)
+        return result, self.new_image
+
+
+    def release(self):
+         #  End acquisition
+            #
+            #  *** NOTES ***
+            #  Ending acquisition appropriately helps ensure that devices clean up
+            #  properly and do not need to be power-cycled to maintain integrity.    
+        self.cam.EndAcquisition()
+        # Deinitialize camera
+        self.cam.DeInit()
+        # Clear camera list before releasing system
+        self.cam_list.Clear()
+
+        # Release system instance
+        self.system.ReleaseInstance()
+
+        # input('Done! Press Enter to exit...')
+        return False
 
 # if __name__ == '__main__':
 #     import sys
--- presentation/main.py
+++ presentation/main.py
@@ -8,7 +8,11 @@ from numpy import uint8
 sys.path.append('../')
 
 from apps.app_module_cv2 import AppCV2
-from apps.app_module_spinnaker import AppSpinnaker
+
+try:
+    from apps.app_module_spinnaker import AppSpinnaker
+except ImportError as error:
+    print ("Error de importación", error.msg)  
 
 from PySide6.QtGui import QGuiApplication, QImage
 from PySide6.QtQml import QQmlApplicationEngine
@@ -58,7 +62,6 @@ class MyPanoramicProvider(QQuickImageProvider):
     def __init__(self):#  -> None:
         super(MyPanoramicProvider, self).__init__(QQuickImageProvider.Image)
         self.cvimg = []
-        self.capture = []
         self._build_panoramic = AppCV2()
         self._lock = Lock()
         self._panoramic = np.zeros((640,480,3),dtype="uint8")
@@ -86,22 +89,17 @@ class MyPanoramicProvider(QQuickImageProvider):
 
 class SpinnakerImageProvider(QQuickImageProvider):
 
-    def __init__(self, app_spinnaker) -> None:
+    def __init__(self, capture_object) -> None:
         super(SpinnakerImageProvider, self).__init__(QQuickImageProvider.Image)
         self.cvimg = []
-        
+        self.capture = capture_object
         self._lock = Lock()
-
-        self.capture = app_spinnaker
 
     def requestImage(self, p_str, size, u):
         global new_image
         global ret
-
-        ret = self.capture.main()
-
+        ret, self.frame = self.capture.read()
         with self._lock:
-            self.frame = self.capture.new_image
             new_image = self.frame
         self.height, self.width, self.depth = self.frame.shape
         self.cvimg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
@@ -110,7 +108,7 @@ class SpinnakerImageProvider(QQuickImageProvider):
     
     def finish_capture(self):
         # @TODO: Puede ser que esté conectandose y desconectándose de la cámara en cada captura. Estar atento.
-        # self.capture.release() 
+        self.capture.release() 
         self.frame = np.zeros((640,480,3),dtype="uint8")
         self.height, self.width, self.depth = self.frame.shape
         self.cvimg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
@@ -122,12 +120,15 @@ class SpinnakerPanoramicProvider(QQuickImageProvider):
     def __init__(self) -> None:
         super(SpinnakerPanoramicProvider, self).__init__(QQuickImageProvider.Image)
         self.cvimg = []
-        
-        self._build_panoramic = AppSpinnaker()
+        self._build_panoramic = AppCV2()
+        self._lock = Lock()
+        self._panoramic = np.zeros((640,480,3),dtype="uint8")
+
     def requestImage(self, p_str, size, u):
         global new_image
         global ret
-        self.frame, growing_flag = self._build_panoramic.panoramic_build(new_image, ret) #self.capture.new_image, ret)  
+        with self._lock:
+            self.frame, growing_flag = self._build_panoramic.panoramic_build(new_image, ret) #self.capture.new_image, ret)  
         self.height, self.width, self.depth = self.frame.shape
         self.cvimg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
         self.cvimg = QImage (self.cvimg.data, self.width, self.height, self.width*self.depth, QImage.Format_RGB888)       
@@ -140,6 +141,7 @@ class SpinnakerPanoramicProvider(QQuickImageProvider):
         self.cvimg = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
         self.cvimg = QImage (self.cvimg.data, self.width, self.height, self.width*self.depth, QImage.Format_RGB888)
 
+
 class Controlers(QObject):
 
     def __init__(self) -> None:
@@ -150,28 +152,22 @@ class Controlers(QObject):
         global video_image
         global parnoramic_image
         global engine
-        # global app_camera
         global new_image
         if play:
             if camera_type==0:
                 capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
                 video_image = MyImageProvider(capture)
-                
                 parnoramic_image = MyPanoramicProvider()
-                engine.addImageProvider("myprovider", video_image)
-                engine.addImageProvider("panoprovider", parnoramic_image)
+
             if camera_type == 1:
                 app_camera = AppSpinnaker()
-                video_image_spinnaker = SpinnakerImageProvider(app_camera)
+                app_camera.main()
+                video_image = SpinnakerImageProvider(app_camera)
                 parnoramic_image = SpinnakerPanoramicProvider()
-                engine.addImageProvider("myprovider", video_image_spinnaker)
-                engine.addImageProvider("panoprovider", parnoramic_image)
 
-                # capture = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-                # video_image = MyImageProvider(capture)
-                # parnoramic_image = MyPanoramicProvider()
-                # engine.addImageProvider("myprovider", video_image)
-                # engine.addImageProvider("panoprovider", parnoramic_image)
+            engine.addImageProvider("myprovider", video_image)
+            engine.addImageProvider("panoprovider", parnoramic_image)
+
         else:
             if camera_type==0:
                 parnoramic_image.finish_capture()
@@ -184,14 +180,7 @@ if __name__ == "__main__":
     
     qml_file = os.fspath(Path(__file__).resolve().parent / "main.qml")
     
-    # app_cv2 =  AppCV2()
-    # video_image = MyImageProvider( app_cv2)
-    # parnoramic_image = MyPanoramicProvider( app_cv2 )
     controlers = Controlers()
-    # video_image_spinnaker = SpinnakerImageProvider(AppSpinnaker())
-    # engine.addImageProvider("myprovider", video_image)
-    # engine.addImageProvider("panoprovider", parnoramic_image)
-    # engine.rootObjects()[0].setProperty('controlers', controlers)
 
     engine.load(qml_file)
     engine.rootObjects()[0].setProperty('controlers', controlers)
