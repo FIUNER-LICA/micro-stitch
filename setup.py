4618fcecb304cd26d2fe642e705613dd37339a99
dz <100449246+dzjavier@users.noreply.github.com>
2023-12-11 08:35:22 -0300

    Remove setup.py file

--- setup.py
+++
@@ -1,9 +0,0 @@
-"""Package configuration."""
-from setuptools import find_packages, setup
-
-setup(
-    name="app_cv2",
-    version="0.1",
-    packages=find_packages(where="modules"),
-    package_dir={"": "src"},
-)
